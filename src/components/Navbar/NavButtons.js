5c6bdefa52f551609edd23680c67eff27b2f6ffc
Abdulsamad sadiq <otaikisadiq2804@gmail.com>
2023-05-06 14:25:06 +0100

    feat: add click functionalities

--- src/components/Navbar/NavButtons.js
+++ src/components/Navbar/NavButtons.js
@@ -1,22 +1,37 @@
 import styles from "./NavButtons.module.css";
-
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 export default function NavButtons(props) {
+  const navigate = useNavigate();
+
+  const onSendButtonClick = useCallback(() => {
+    navigate("/send");
+  }, [navigate]);
+  const onHomeButtonClick = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+  const onClaimButtonClick = useCallback(() => {
+    navigate("/claim");
+  }, [navigate]);
   return (
     <div className={styles.frameParent}>
       <div className={styles.homeParent}>
-        <div className={styles.home}>
+        <div className={styles.home} onClick={onHomeButtonClick}>
           <b className={props.active === 1 ? styles.active : ""}>Home</b>
           <div className={styles.frameChild} />
         </div>
-        <div className={styles.home}>
+        <div className={styles.home} onClick={onSendButtonClick}>
           <b className={props.active === 2 ? styles.active : ""}>Send</b>
           <div className={styles.frameChild} />
         </div>
-        <div className={styles.home}>
-          <b className={props.active === 3 ? styles.active : ""}>Claim</b>
-          <span className={props.notifAmount ? styles.badge : ""}>
-            {props.notifAmount}
-          </span>
+        <div className={styles.home} onClick={onClaimButtonClick}>
+          <b className={props.active === 3 ? styles.active : ""}>
+            Claim
+            <span className={props.notifAmount ? styles.badge : ""}>
+              {props.notifAmount}
+            </span>
+          </b>
+
           <div className={styles.frameChild} />
         </div>
       </div>
