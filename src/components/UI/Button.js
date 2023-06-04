20b113cf7799241abc7cddf5f3a429a7a177d743
Abdulsamad sadiq <otaikisadiq2804@gmail.com>
2023-06-04 16:15:18 +0100

    feat: Add Account setup screen

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,13 @@
 import styles from "./Button.module.css";
 export default function Button(props) {
   return (
-    <button className={props.isDark ? styles.darkButton : styles.whiteButton}>
+    <button
+      className={
+        props.isDark
+          ? `${styles.darkButton} ${props.extraStyle}`
+          : `${styles.whiteButton} ${props.extraStyle}`
+      }
+    >
       <b>{props.btnText}</b>
     </button>
   );
